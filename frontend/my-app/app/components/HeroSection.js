80d6dea49e634244c92156c30aa33ff09166d585
Amna Afzal <amnaafzal605@gmail.com>
2025-09-10 22:33:53 +0500

    added blogs section

--- frontend/my-app/app/components/HeroSection.js
+++ frontend/my-app/app/components/HeroSection.js
@@ -28,7 +28,7 @@ const HeroSection = () => {
 
     return (
         <>
-            <section className='my-24 section gap-7'>
+            <section className='my-24 section gap-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
 
                 
                     {cards.map((card)=>
--- frontend/my-app/app/page.js
+++ frontend/my-app/app/page.js
@@ -5,7 +5,8 @@ import HeroSection from './components/HeroSection'
 import Trending_section from './shop/Trending_section'
 import Deals_section from './components/Deals_section'
 import Promo_banner from './components/Promo_banner'
-import Latest_section from './components/Latest_section'
+import Blogs from './blogs/Blogs'
+
 
 const page = () => {
   return (
@@ -16,7 +17,7 @@ const page = () => {
       <Trending_section />
       <Deals_section />
       <Promo_banner />
-      <Latest_section />
+      <Blogs />
     </div>
   )
 }
