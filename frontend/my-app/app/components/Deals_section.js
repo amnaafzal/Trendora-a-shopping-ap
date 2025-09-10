d17ef254e7128ecdeaa31c736524b04ba1892198
Amna Afzal <amnaafzal605@gmail.com>
2025-09-10 20:18:59 +0500

    Deals section completed

--- frontend/my-app/app/components/Deals_section.js
+++ frontend/my-app/app/components/Deals_section.js
@@ -2,18 +2,41 @@ import React from 'react'
 import Image from 'next/image'
 
 const Deals_section = () => {
-  return (
-    <div className='mt-10'>
-      <section className="deals section relative w-full ">
-        <Image src='/imgaes/banner-3.jpg' alt='deals_image' height={1000} width={1100} className='rounded-2xl max-w-[90%] w-[70%]' ></Image>
-        <div className="deals_content absolute top-[20%] sm:top-[30%] md:top-[20%] left-[20%] w-[40%] md:w-[30%]">
-           <p className='text-sm text-pink-500'>Get Up To 30% Discount</p>
-           <h1 className='text-xl sm:text-2xl font-bold'>Deal Of The Month</h1>
-           <p className='hidden md:block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem cum nisi. Veritatis velit perspiciatis ullam, doloremque qui eaque tempora in</p>
+    return (
+        <div className='mt-10'>
+            <section className="deals section relative w-full ">
+                <Image src='/imgaes/banner-3.jpg' alt='deals_image' height={1000} width={1100} className='rounded-2xl max-w-[90%] w-[70%]' ></Image>
+                <div className="deals_description absolute top-[20%] sm:top-[30%] md:top-[20%] left-[20%] w-[40%] md:w-[30%]">
+                    <div className='deals_content'>
+                        <p className='text-sm text-pink-500'>Get Up To 30% Discount</p>
+                        <h1 className='text-xl sm:text-2xl font-bold'>Deal Of The Month</h1>
+                        <p className='hidden md:block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem cum nisi. Veritatis velit perspiciatis ullam, doloremque qui eaque tempora in</p>
+                    </div>
+                    <div className="deals_countdown flex justify-around gap-3 mt-14 w-[70%]">
+                        <div className="countdown_card">
+                            <h2>2</h2>
+                            <p>Days</p>
+                        </div>
+                        <div className="countdown_card">
+                            <h2>12</h2>
+                            <p>Hrs</p>
+                        </div>
+                        <div className="countdown_card">
+                            <h2>20</h2>
+                            <p>Mins</p>
+                        </div>
+                        <div className="countdown_card">
+                            <h2>25</h2>
+                            <p>Sec</p>
+                        </div>
+                        
+                    </div>
+
+
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  )
+    )
 }
 
 export default Deals_section
