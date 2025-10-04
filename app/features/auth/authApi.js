e0174bbd89ff917cf0520d8c427b556295005dbf
Amna Afzal <amnaafzal605@gmail.com>
2025-10-04 15:19:26 +0500

    created logout, updateRole, updateProfile, deleteUser and getUser API

--- app/features/auth/authApi.js
+++ app/features/auth/authApi.js
@@ -20,7 +20,50 @@ const authApi = createApi({
                 method: 'POST',
                 body: credentials
             })
-        })
+        }),
+        logoutUser: builder.mutation({
+            query: () => ({
+                url: '/logout',
+                method: "POST"
+            })
+        }),
+
+        getUser: builder.query({
+            query: () => ({
+                url: '/users',
+                method: "GET"
+            }),
+            invalidatesTags: ["users"],
+            refetchedOnMount: true
+        }),
+
+        deleteUser: builder.mutation({
+            query: (userId) => ({
+                url: `/users/${userId}`,
+                method: "DELETE"
+            }),
+            invalidatesTags: ["users"]
+        }),
+
+        updateRole: builder.mutation({
+            query : ({userId, role}) => ({
+                url: `/user/${userId}`,
+                method: 'PUT',
+                body: {role}
+            }),
+            refetchedOnMount : true,
+            invalidatesTags: ['users']
+        }),
+
+        updateProfile : builder.mutation({
+            query: (user) => ({
+                url: '/edit-profile',
+                method: "PATCH",
+                body: user
+            })
+        }),
+
+
     })
 
 
@@ -29,5 +72,5 @@ const authApi = createApi({
 })
 
 
-export const {useLoginUserMutation, useRegisterUserMutation} = authApi;
+export const {useLoginUserMutation, useRegisterUserMutation, useLogoutUserMutation, useDeleteUserMutation, useUpdateProfileMutation, useUpdateRoleMutation, useGetUserQuery} = authApi;
 export default authApi; 
